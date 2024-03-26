5b52492114a8e596b084a2c81a0ee168ed44f900
Elvira Tidemo <elvir@MacBook-Air.local>
2024-03-26 19:49:01 +0100

    Add limitations to profile fields

--- crafts_profiles/models.py
+++ crafts_profiles/models.py
@@ -2,15 +2,14 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 
-
 class Profile(models.Model):
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    name = models.CharField(max_length=255, blank=True)
-    content = models.TextField(blank=True)
+    name = models.CharField(max_length=255, null=True, blank=True)
+    content = models.TextField(max_length=500, null=True, blank=True)
     image = models.ImageField(
-        upload_to='images/', default='../default_profile_qdjgyp'
+        upload_to='images/', default='../default_profile_rkxcnv'
     )
 
     class Meta:
