2c928cb80edecd2b3be5eef7db48b74428f49b59
Elvira Tidemo <elvir@MacBook-Air.local>
2024-03-26 18:12:18 +0100

    Add allowed hosts, heroku

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -78,7 +78,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1' ]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://world-of-craft-0e06bf8581a1.herokuapp.com/']
 
 
 # Application definition
